f2581ed22627864dda80a5fdbde1d1f00079b1f3
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2020-10-28 12:33:55 -0300

    28/10/20

--- Portao2020.ino
+++ Portao2020.ino
@@ -1,4 +1,3 @@
-
 #include <SPI.h>
 #include <MFRC522.h>
 #include <WiFi.h>
@@ -14,11 +13,9 @@
 const char* ssid = "Thuliv";
 const char* password = "90iojknm";
 
-//Parâmetros de rede
 IPAddress ip(192, 168, 1, 253);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
-const uint32_t PORTA = 80; //A porta que será utilizada (padrão 80)
 
 //Algumas informações que podem ser interessantes
 const uint32_t chipID = (uint32_t)(ESP.getEfuseMac() >> 32); //um ID exclusivo do Chip...
@@ -28,14 +25,10 @@ const String VERSION = "<p> Versão: 1.0 </p>"; //Exemplo de um controle de vers
 //Informações interessantes agrupadas
 const String INFOS = VERSION + CHIP_ID;
 
-//Sinalizador de autorização do OTA
 boolean OTA_AUTORIZADO = false;
 
-//inicia o servidor na porta selecionada
-//aqui testamos na porta 3000, ao invés da 80 padrão
-WebServer server(PORTA);
+WebServer server(80);
 
-//Páginas HTML utilizadas no procedimento OTA
 String index1 =
 "<!DOCTYPE html>"
   "<html>"
@@ -100,13 +93,12 @@ void setup(void)
 {
   pinMode(pinFechadura, OUTPUT);
   
-  Serial.begin(115200); //Serial para debug
+  Serial.begin(115200); 
 
-  WiFi.mode(WIFI_AP_STA); //Comfigura o ESP32 como ponto de acesso e estação
+  WiFi.mode(WIFI_AP_STA); 
 
-  WiFi.begin(ssid, password);// inicia a conexão com o WiFi
+  WiFi.begin(ssid, password);
 
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -121,65 +113,51 @@ void setup(void)
   MDNS.begin("talkinghome");
  
 
-  if (WiFi.status() == WL_CONNECTED) //aguarda a conexão
+  if (WiFi.status() == WL_CONNECTED) 
   {
-    //atende uma solicitação para a raiz
-    // e devolve a página 'verifica'
-    server.on("/", HTTP_GET, []() //atende uma solicitação para a raiz
+    server.on("/", HTTP_GET, []() 
     {
       server.sendHeader("Connection", "close");
       server.send(200, "text/html", index1);
     });
 
-    //atende uma solicitação para a página avalia
     server.on("/arquivo", HTTP_POST, [] ()
     {
-      Serial.println("Em server.on /avalia: args= " + String(server.arg("autorizacao"))); //somente para debug
+      Serial.println("Em server.on /avalia: args= " + String(server.arg("autorizacao"))); 
 
-      if (server.arg("autorizacao") != "90iojknm") // confere se o dado de autorização atende a avaliação
+      if (server.arg("autorizacao") != "90iojknm") 
       {
-        //se não atende, serve a página indicando uma falha
         server.sendHeader("Connection", "close");
         server.send(200, "text/html", chaveIncorreta);
-        //ESP.restart();
       }
       else
       {
-        //se atende, solicita a página de índice do servidor
-        // e sinaliza que o OTA está autorizado
         OTA_AUTORIZADO = true;
         server.sendHeader("Connection", "close");
         server.send(200, "text/html", index2);
       }
     });
 
-    //serve a página de indice do servidor
-    //para seleção do arquivo
     server.on("/index2", HTTP_GET, []()
     {
       server.sendHeader("Connection", "close");
       server.send(200, "text/html", index2);
     });
 
-    //tenta iniciar a atualização . . .
     server.on("/update", HTTP_POST, []()
     {
-      //verifica se a autorização é false.
-      //Se for falsa, serve a página de erro e cancela o processo.
       if (OTA_AUTORIZADO == false)
       {
         server.sendHeader("Connection", "close");
         server.send(200, "text/html", chaveIncorreta);
         return;
       }
-      //Serve uma página final que depende do resultado da atualização
       server.sendHeader("Connection", "close");
       server.send(200, "text/html", (Update.hasError()) ? chaveIncorreta : atualizado);
       delay(1000);
       ESP.restart();
     }, []()
     {
-      //Mas estiver autorizado, inicia a atualização
       HTTPUpload& upload = server.upload();
       if (upload.status == UPLOAD_FILE_START)
       {
@@ -187,7 +165,6 @@ void setup(void)
         Serial.printf("Atualizando: %s\n", upload.filename.c_str());
         if (!Update.begin())
         {
-          //se a atualização não iniciar, envia para serial mensagem de erro.
           Update.printError(Serial);
         }
       }
@@ -195,7 +172,6 @@ void setup(void)
       {
         if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
         {
-          //se não conseguiu escrever o arquivo, envia erro para serial
           Update.printError(Serial);
         }
       }
@@ -203,19 +179,16 @@ void setup(void)
       {
         if (Update.end(true))
         {
-          //se finalizou a atualização, envia mensagem para a serial informando
           Serial.printf("Atualização bem sucedida! %u\nReiniciando...\n", upload.totalSize);
         }
         else
         {
-          //se não finalizou a atualização, envia o erro para a serial.
           Update.printError(Serial);
         }
         Serial.setDebugOutput(false);
       }
       else
       {
-        //se não conseguiu identificar a falha no processo, envia uma mensagem para a serial
         Serial.printf("Atualização falhou inesperadamente! (possivelmente a conexão foi perdida.): status=%d\n", upload.status);
       }
     });
